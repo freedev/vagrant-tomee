c62f99279a8bbbcb354c3dc6fb2c64b389b81fa5
Vincenzo D'Amore <v.damore@gmail.com>
2015-02-20 00:03:40 +0100

    updated tomee dependency

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Vagrant Installs/Configures TomEE'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'tomee', '~> 0.10.0'
+depends 'tomee', '~> 0.14.0'
 
 supports "debian", ">= 0.0.0"
 supports "ubuntu", ">= 0.0.0"
